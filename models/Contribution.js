d7a2281a5a09d6e019f70588e094d508be164a62
Shihab5500 <Shihabahmed6501@gmail.com>
2025-11-10 10:07:31 +0600

    again update

--- models/Contribution.js
+++ models/Contribution.js
@@ -6,12 +6,12 @@ const contributionSchema = new mongoose.Schema({
   issueId: { type: String, required: true },
   amount: { type: Number, required: true },
   name: { type: String, required: true },
-  email: { type: String, required: true }, // যিনি কন্ট্রিবিউট করছেন তার ইমেইল (লগইন থেকে)
+  email: { type: String, required: true }, 
   phone: { type: String },
   address: { type: String },
   additionalInfo: { type: String },
   date: { type: Date, default: Date.now },
-  userId: { type: String, required: true } // লগইন করা ইউজারের Firebase UID
+  userId: { type: String, required: true } 
 },{timestamps:true});
 
 export default mongoose.model('Contribution', contributionSchema);
